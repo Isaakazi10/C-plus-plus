eb7f5255f7e8054721cdc283be541c318db342b8
isaa kazi <isaakazi10@gmail.com>
2024-02-01 10:34:34 +0530

    Sales

---
+++ Sales/sales.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item var, sum;
+    int count=1;
+
+    if (std::cin >> sum)
+    {
+        while ((std::cin >> var))
+        {
+            if (sum.isbn() != var.isbn())
+            {
+                std::cout << "\n" << sum.isbn() << " " << count << "\n" << std::endl;
+                sum = var;
+                count=1;
+            }
+            else
+            {
+                sum += var;
+                count++;
+            }
+        }     
+    }
+    else
+    {
+        std::cerr << "No input" << std::endl;
+        return -1;
+    }
+    std::cout << "\n" << sum.isbn() << " " << count << "\n" << std::endl;
+}
